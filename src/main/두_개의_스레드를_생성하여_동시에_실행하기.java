2342c14774d55dce56776404692e91b4a852afcc
chinoel <tnehd83@naver.com>
2025-03-12 09:32:29 +0900

    feat:완료한 것

--- src/main/두_개의_스레드를_생성하여_동시에_실행하기.java
+++ src/main/두_개의_스레드를_생성하여_동시에_실행하기.java
@@ -1,4 +1,29 @@
 package main;
 
+class HelloThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("hello " + i);
+        }
+    }
+}
+
+class ByeThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("bye " + i);
+        }
+    }
+}
+
 public class 두_개의_스레드를_생성하여_동시에_실행하기 {
+    public static void main(String[] args) {
+        HelloThread helloThread = new HelloThread();
+        ByeThread byeThread = new ByeThread();
+
+        helloThread.start();
+        byeThread.start();
+    }
 }
--- src/main/추상_클래스와_인터페이스를_활용한_도형_프로그램_작성하기.java
+++ src/main/추상_클래스와_인터페이스를_활용한_도형_프로그램_작성하기.java
@@ -1,4 +1,47 @@
 package main;
 
+abstract class Shape {
+    abstract double getArea();
+}
+
+interface Measurable {
+    double getArea();
+}
+
+class Circle extends Shape implements Measurable {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape implements Measurable {
+    private final double width;
+    private final double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+}
+
 public class 추상_클래스와_인터페이스를_활용한_도형_프로그램_작성하기 {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(5, 5);
+
+        System.out.println(circle.getArea());
+        System.out.println(rectangle.getArea());
+    }
 }
