dfb94cc206a71748dbf45a1fcc5107d9a78efff9
chinoel <tnehd83@naver.com>
2025-03-28 13:52:38 +0900

    first commit

---
+++ src/main/java/org/chinoel/goormspring/GoormHighSpringLevelApplication.java
@@ -0,0 +1,13 @@
+package org.chinoel.goormspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormHighSpringLevelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormHighSpringLevelApplication.class, args);
+    }
+
+}
