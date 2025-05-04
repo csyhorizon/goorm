006b06643cf385f1640202f91b57cc0df90db453
chino <tnehd83@naver.com>
2025-05-04 22:18:02 +0900

    5/4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,6 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-
     public static void main(String[] args) throws IOException {
 
     }
