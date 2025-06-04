732bd472517c5f531a9ac1f29811d2468ae7fea8
yoon <jiys5799@gmail.com>
2025-06-05 02:57:02 +0900

    Set project

---
+++ src/main/java/uniqram/c1one/C1oneApplication.java
@@ -0,0 +1,13 @@
+package uniqram.c1one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1oneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1oneApplication.class, args);
+	}
+
+}
