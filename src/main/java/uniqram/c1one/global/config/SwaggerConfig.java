748b546bf07e80f5c9f11efe8d8c9588bbbad31a
ynyejin <yyj030625@gmail.com>
2025-06-11 01:42:39 +0900

    feat: Swagger 및 MySQL 초기 설정 완료

---
+++ src/main/java/uniqram/c1one/global/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package uniqram.c1one.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Uniqram Swagger")
+                .description("인스타그램 클론 프로젝트 API 명세서")
+                .version("1.0.0");
+    }
+}
