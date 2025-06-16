b84c1e35c2031acc60632a506a579db0798c39b4
yoon <jiys5799@gmail.com>
2025-06-16 11:13:03 +0900

    user initial setting

---
+++ src/main/java/uniqram/c1one/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package uniqram.c1one.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uniqram.c1one.user.entity.Users;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByUsername(String username);
+}
