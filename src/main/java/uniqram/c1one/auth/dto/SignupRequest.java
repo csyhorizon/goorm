99bdf5df3d50f7ea37ca070533d5b93ce0cc95a7
jh <biruk23@naver.com>
2025-06-17 10:43:21 +0900

    Refactor: SignupRequest에 Lombok 적용 및 검증 로직 일부 반영

--- src/main/java/uniqram/c1one/auth/dto/SignupRequest.java
+++ src/main/java/uniqram/c1one/auth/dto/SignupRequest.java
@@ -1,5 +1,8 @@
 package uniqram.c1one.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,8 +11,19 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class SignupRequest {
+
+    @NotBlank
+    @Size(min = 2, max = 20)
     private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 100)
     private String password;
+
+    @NotBlank
     private String confirmPassword;
+
+    @NotBlank
+    @Email
     private String email;
 }
