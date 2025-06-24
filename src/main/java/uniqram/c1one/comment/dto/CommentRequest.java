982a870fb8fe99b8b438c205d730f0820390683e
Altair5869 <th0538@naver.com>
2025-06-24 11:58:55 +0900

    Feat: CommentRequest, CommentResponse 생성 / CommentRepository findByPost, findByParentComment 메소드 생성

---
+++ src/main/java/uniqram/c1one/comment/dto/CommentRequest.java
@@ -0,0 +1,15 @@
+package uniqram.c1one.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentRequest {
+    private Long userId; //임시 포함
+    private Long postId;
+    private Long parentCommentId; //대댓글인 경우
+    private String content;
+}
