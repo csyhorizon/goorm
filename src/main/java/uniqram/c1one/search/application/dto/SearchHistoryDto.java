0f3629ab4e43f1bbecc8f20e9fa637ade5cb1d27
currysoda <currysoda1598@gmail.com>
2025-06-25 01:04:26 +0900

    패키지 구조 변경

---
+++ src/main/java/uniqram/c1one/search/application/dto/SearchHistoryDto.java
@@ -0,0 +1,9 @@
+package uniqram.c1one.search.application.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SearchHistoryDto {
+
+	List<>
+}
---
+++ src/main/java/uniqram/c1one/search/domain/entity/SearchType.java
@@ -0,0 +1,7 @@
+package uniqram.c1one.search.domain.entity;
+
+public enum SearchType {
+	
+	HashTagSearch, UserSearch
+	
+}
