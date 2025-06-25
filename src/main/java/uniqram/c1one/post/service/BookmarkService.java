ab7a5df959e912768d8ebc99d9c86301ddabe9d3
ynyejin <yyj030625@gmail.com>
2025-06-25 13:51:55 +0900

    feat: 북마크 한 게시글 조회 기능 구현

--- src/main/java/uniqram/c1one/post/service/BookmarkService.java
+++ src/main/java/uniqram/c1one/post/service/BookmarkService.java
@@ -3,12 +3,16 @@ package uniqram.c1one.post.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uniqram.c1one.post.dto.BookmarkPostResponse;
 import uniqram.c1one.post.entity.Bookmark;
 import uniqram.c1one.post.entity.Post;
 import uniqram.c1one.post.exception.PostErrorCode;
 import uniqram.c1one.post.exception.PostException;
 import uniqram.c1one.post.repository.BookmarkRepository;
+import uniqram.c1one.post.repository.PostMediaRepository;
 import uniqram.c1one.post.repository.PostRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +20,7 @@ public class BookmarkService {
 
     private final BookmarkRepository bookmarkRepository;
     private final PostRepository postRepository;
+    private final PostMediaRepository postMediaRepository;
 
     @Transactional
     public boolean bookmark(Long userId, Long postId) {
@@ -31,5 +36,20 @@ public class BookmarkService {
             bookmarkRepository.save(bookmark);
             return true; // 등록
         }
+    }
+
+    @Transactional(readOnly = true) // TODO: 북마크와 게시글(Post) 간 N+1 문제
+    public List<BookmarkPostResponse> getMyBookmarks(Long userId) {
+        List<Bookmark> bookmarks = bookmarkRepository.findByUserId(userId);
+
+        return bookmarks.stream()
+                .map(bookmark -> {
+                    Post post = bookmark.getPost();
+                    String repImageUrl = postMediaRepository.findFirstByPostIdOrderByIdAsc(post.getId())
+                            .orElseThrow(() -> new IllegalStateException("대표 이미지가 존재하지 않습니다."))
+                            .getMediaUrl();
+                    return BookmarkPostResponse.of(post.getId(), repImageUrl);
+                })
+                .toList();
     }
 }
