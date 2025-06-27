b882661af52b21195008c0e9396745c8e2d69393
jh <biruk23@naver.com>
2025-06-27 16:47:51 +0900

    Test: AuthService 회원가입 및 인가 테스트 코드 작성

--- src/main/java/uniqram/c1one/global/config/SecurityConfig.java
+++ src/main/java/uniqram/c1one/global/config/SecurityConfig.java
@@ -46,8 +46,7 @@ public class SecurityConfig {
                                 "/api/auth/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/**" // 개발 끝나면 삭제 예정.
+                                "/swagger-ui.html"
                         ).permitAll()
                         .requestMatchers("/api/user/**").hasRole("USER")
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
