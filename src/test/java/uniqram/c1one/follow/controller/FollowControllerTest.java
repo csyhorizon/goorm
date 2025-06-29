e649f67111a50bb8d6f1729fedb9d5ea36bd2fc3
csy <tnehd83@naver.com>
2025-06-29 23:11:32 +0900

    feat:팔로우 관련 테스트 케이스 작성

--- src/test/java/uniqram/c1one/follow/controller/FollowControllerTest.java
+++
@@ -1,7 +0,0 @@
-package uniqram.c1one.follow.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class FollowControllerTest {
-
-}
--- src/test/java/uniqram/c1one/follow/service/FollowServiceTest.java
+++ src/test/java/uniqram/c1one/follow/service/FollowServiceTest.java
@@ -1,7 +1,193 @@
 package uniqram.c1one.follow.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import uniqram.c1one.follow.entity.Follow;
+import uniqram.c1one.follow.exception.FollowErrorCode;
+import uniqram.c1one.follow.exception.FollowException;
+import uniqram.c1one.follow.repository.FollowRepository;
+import uniqram.c1one.user.entity.Role;
+import uniqram.c1one.user.entity.Users;
+import uniqram.c1one.user.repository.UserRepository;
+
+@SpringBootTest
+@Transactional
 class FollowServiceTest {
 
+    @Autowired
+    private FollowService followService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private FollowRepository followRepository;
+
+    private Users createUser(String username) {
+        Users users = Users.builder()
+                .username(username)
+                .password("password")
+                .email(username + "@test.com")
+                .role(Role.USER)
+                .build();
+        return userRepository.save(users);
+    }
+
+    @Test
+    @DisplayName("성공 - 팔로우 생성")
+    void createFollow_success() {
+        Users user1 = createUser("user1");
+        Users user2 = createUser("user2");
+
+        followService.createFollow(user1.getId(), user2.getId());
+
+        List<Follow> followings = followService.getFollowings(user1.getId());
+        assertEquals(1, followings.size());
+        assertEquals(user2.getId(), followings.get(0).getFollowing().getId());
+    }
+
+    @Test
+    @DisplayName("실패 - 팔로우 생성: 존재하지 않는 사용자")
+    void createFollow_fail_userNotFound() {
+        // when
+        FollowException ex = assertThrows(FollowException.class, () ->
+                followService.createFollow(999L, 1000L)
+        );
+
+        // then
+        assertEquals(FollowErrorCode.USER_NOT_FOUND, ex.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("실패 - 팔로우 생성: 자기 자신 팔로우 시도")
+    void createFollow_fail_cannotFollowSelf() {
+        // given
+        Users user = createUser("myself");
+
+        // when
+        FollowException ex = assertThrows(FollowException.class, () ->
+                followService.createFollow(user.getId(), user.getId()));
+
+        // then
+        assertEquals(FollowErrorCode.CANNOT_FOLLOW_SELF, ex.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("실패 - 팔로우 생성: 이미 팔로우 한 사용자")
+    void createFollow_fail_alreadyFollowing() {
+        // given
+        Users user1 = createUser("user1");
+        Users user2 = createUser("user2");
+        followService.createFollow(user1.getId(), user2.getId());
+
+        // when
+        FollowException ex = assertThrows(FollowException.class, () ->
+                followService.createFollow(user1.getId(), user2.getId()));
+
+        //hen
+        assertEquals(FollowErrorCode.ALREADY_FOLLOWING, ex.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("성공 - 언팔로우")
+    void unfollow_success() {
+        // given
+        Users user1 = createUser("user1");
+        Users user2 = createUser("user2");
+        followService.createFollow(user1.getId(), user2.getId());
+
+        // when
+        followService.unfollow(user1.getId(), user2.getId());
+
+        // then
+        assertEquals(0, followService.getFollowings(user1.getId()).size());
+    }
+
+    @Test
+    @DisplayName("실패 - 언팔로우: 팔로우 관계 없음")
+    void unfollow_fail_followNotFound() {
+        // given
+        Users user1 = createUser("user1");
+        Users user2 = createUser("user2");
+
+        // when
+        FollowException ex = assertThrows(FollowException.class, () ->
+                followService.unfollow(user1.getId(), user2.getId())
+        );
+
+        // then
+        assertEquals(FollowErrorCode.FOLLOW_NOT_FOUND, ex.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("성공 - 내 팔로워 강제 삭제")
+    void removeFollower_success() {
+        // given
+        Users user1 = createUser("user1");
+        Users user2 = createUser("user2");
+        followService.createFollow(user2.getId(), user1.getId()); // user2가 user1을 팔로우
+
+        // when
+        followService.removeFollower(user1.getId(), user2.getId());
+
+        // then
+        assertEquals(0, followService.getFollowers(user1.getId()).size());
+    }
+
+    @Test
+    @DisplayName("실패 - 내 팔로워 강제 삭제: 팔로우 관계 없음")
+    void removeFollower_fail_followNotFound() {
+        // given
+        Users user1 = createUser("user1");
+        Users user2 = createUser("user2");
+
+        // when
+        FollowException ex = assertThrows(FollowException.class, () ->
+                followService.removeFollower(user1.getId(), user2.getId())
+        );
+
+        // then
+        assertEquals(FollowErrorCode.FOLLOW_NOT_FOUND, ex.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("성공 - 팔로잉 목록 조회")
+    void getFollowings_success() {
+        // given
+        Users user1 = createUser("user1");
+        Users user2 = createUser("user2");
+        Users user3 = createUser("user3");
+        followService.createFollow(user1.getId(), user2.getId());
+        followService.createFollow(user1.getId(), user3.getId());
+
+        // when
+        List<Follow> followings = followService.getFollowings(user1.getId());
+
+        // then
+        assertEquals(2, followings.size());
+    }
+
+    @Test
+    @DisplayName("성공 - 팔로워 목록 조회")
+    void getFollowers_success() {
+        // given
+        Users user1 = createUser("user1");
+        Users user2 = createUser("user2");
+        Users user3 = createUser("user3");
+        followService.createFollow(user2.getId(), user1.getId());
+        followService.createFollow(user3.getId(), user1.getId());
+
+        // when
+        List<Follow> followers = followService.getFollowers(user1.getId());
+
+        // then
+        assertEquals(2, followers.size());
+    }
 }
