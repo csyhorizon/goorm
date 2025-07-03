d5b197e5616ea05566e6cf76ac1d0ee1e4b4ffec
ynyejin <yyj030625@gmail.com>
2025-07-03 17:59:22 +0900

    refactor: 게시글 좋아요 수 조회 로직 Redis 적용 및 댓글 관련 로직 추가

--- src/main/java/uniqram/c1one/post/dto/PostDetailResponse.java
+++ src/main/java/uniqram/c1one/post/dto/PostDetailResponse.java
@@ -2,6 +2,7 @@ package uniqram.c1one.post.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import uniqram.c1one.comment.dto.CommentResponse;
 import uniqram.c1one.post.entity.Post;
 
 import java.util.List;
@@ -22,13 +23,14 @@ public class PostDetailResponse {
     private boolean likedByMe;
 
     private int commentCount;
-    private List<CommentDto> comments;
+    private List<CommentResponse> comments;
 
     public static PostDetailResponse from(Post post,
                                         List<String> mediaUrl,
                                         int likeCount,
                                         List<LikeUserDto> likeUsers,
-                                        boolean likedByMe) {
+                                        boolean likedByMe,
+                                        List<CommentResponse> comments ) {
         return PostDetailResponse.builder()
                 .postId(post.getId())
                 .content(post.getContent())
@@ -38,7 +40,7 @@ public class PostDetailResponse {
                 .likeUsers(likeUsers)
                 .likedByMe(likedByMe)
 //                .commentCount(commentCount)
-//                .comments(comments)
+                .comments(comments)
                 .memberId(post.getUser().getId())
                 .username(post.getUser().getUsername())
                 .build();
