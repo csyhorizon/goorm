2c0572afa70f270677f0edd35e11d8d891381a4f
csy <tnehd83@naver.com>
2025-07-04 10:42:37 +0900

    refactor:controller is DTO -> AuthenticationPrincipal

--- src/main/java/uniqram/c1one/follow/controller/FollowController.java
+++ src/main/java/uniqram/c1one/follow/controller/FollowController.java
@@ -3,16 +3,16 @@ package uniqram.c1one.follow.controller;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uniqram.c1one.follow.dto.FollowDto;
-import uniqram.c1one.follow.dto.FollowRequestDto;
 import uniqram.c1one.follow.service.FollowService;
+import uniqram.c1one.security.adapter.CustomUserDetails;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,32 +21,30 @@ public class FollowController {
 
     private final FollowService followService;
 
-    // 모든 DTO는 자신 호출의 임시 방편이고, @AuthenticationPrincipal로 추후에 변경 필요
-
     @PostMapping("follows/{targetUserId}")
     public ResponseEntity<Void> createFollow(
             @PathVariable Long targetUserId,
-            @RequestBody FollowRequestDto requestDto
+            @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
-        followService.createFollow(requestDto.getMyId(), targetUserId);
+        followService.createFollow(userDetails.getUserId(), targetUserId);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("follows/{targetUserId}")
     public ResponseEntity<Void> unfollow(
-            @RequestBody FollowRequestDto requestDto,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long targetUserId
     ) {
-        followService.unfollow(requestDto.getMyId(), targetUserId);
+        followService.unfollow(userDetails.getUserId(), targetUserId);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("followers/{targetUserId}")
     public ResponseEntity<Void> removeFollower(
-            @RequestBody FollowRequestDto requestDto,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long targetUserId
     ) {
-        followService.removeFollower(requestDto.getMyId(), targetUserId);
+        followService.removeFollower(userDetails.getUserId(), targetUserId);
         return ResponseEntity.noContent().build();
     }
 
