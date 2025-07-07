8ce3b461484ab905adc15281db513250eca9d2b2
Altair5869 <th0538@naver.com>
2025-07-07 15:04:31 +0900

    Feat: DM 관련 도메인 개발

---
+++ src/main/java/uniqram/c1one/dm/repository/ChatRoomRepository.java
@@ -0,0 +1,13 @@
+package uniqram.c1one.dm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uniqram.c1one.dm.entity.ChatRoom;
+
+import java.util.List;
+
+@Repository
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    List<ChatRoom> findAllByUserId(Long userId);
+}
