7cfcf9e10546a11f1e7fd8d43cc15187a9928fba
jh <biruk23@naver.com>
2025-07-09 10:57:37 +0900

    Refactor: Redis 기반으로 대시보드 접속자 통계 및 목록 기능 개선

--- src/main/java/uniqram/c1one/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/uniqram/c1one/security/jwt/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import uniqram.c1one.redis.service.ActiveUserService;
+import uniqram.c1one.security.adapter.CustomUserDetails;
 
 import java.io.IOException;
 
@@ -23,16 +24,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         // Request Header 에서 jwt 토큰 추출
         String token = resolveToken(request);
-        validToken(token);
+        validToken(token, request);
         filterChain.doFilter(request, response);
     }
 
-    private void validToken(String token) {
+    private void validToken(String token, HttpServletRequest request) {
         // 토큰 유효성 검증
         if (token != null && jwtTokenProvider.validateToken(token)) {
             // 인증 정보 생성 및 저장
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            // 인증 성공 시 Redis에 접속 정보 등록
+            if (authentication.getPrincipal() instanceof CustomUserDetails userDetails) {
+                String ip = request.getRemoteAddr();
+                activeUserService.addActiveUser(userDetails, ip);
+            }
         }
     }
 
