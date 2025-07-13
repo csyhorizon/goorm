eb209b8e46f3681551a7afd5f0233a97e38d5d08
ynyejin <yyj030625@gmail.com>
2025-07-13 23:35:07 +0900

    feat: dto 필드 추가

--- src/main/java/uniqram/c1one/post/dto/HomePostResponse.java
+++ src/main/java/uniqram/c1one/post/dto/HomePostResponse.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import uniqram.c1one.comment.dto.CommentResponse;
 import uniqram.c1one.post.entity.Post;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -26,6 +27,8 @@ public class HomePostResponse {
 
     private List<CommentResponse> comments;
 
+    private LocalDateTime createdAt;
+
     public static HomePostResponse from(Post post,
                                         List<String> mediaUrl,
                                         int likeCount,
@@ -45,6 +48,7 @@ public class HomePostResponse {
                 .comments(comments)
                 .memberId(post.getUser().getId())
                 .username(post.getUser().getUsername())
+                .createdAt(post.getCreatedAt())
                 .build();
     }
 }
