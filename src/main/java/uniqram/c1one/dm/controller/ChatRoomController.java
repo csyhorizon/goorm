4282f9efbfb0548a0e6110ec6e7053d6c658319c
Altair5869 <th0538@naver.com>
2025-07-14 15:53:10 +0900

    Refactor: DM 도메인 Swagger 상세 설명

--- src/main/java/uniqram/c1one/dm/controller/ChatRoomController.java
+++ src/main/java/uniqram/c1one/dm/controller/ChatRoomController.java
@@ -1,5 +1,7 @@
 package uniqram.c1one.dm.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,6 +20,8 @@ public class ChatRoomController {
     private final ChatRoomService chatRoomService;
     private final ChatMessageService chatMessageService;
 
+    @Operation(summary = "채팅방 생성", description = "채팅방을 생성합니다.")
+    @ApiResponse(responseCode = "201", description = "셍성 성공")
     @PostMapping
     public ResponseEntity<ChatRoomCreateResponse> createChatRoom(
             @RequestBody ChatRoomCreateRequest request,
@@ -26,6 +30,8 @@ public class ChatRoomController {
         return ResponseEntity.ok(chatRoomService.createChatRoom(userId, request));
     }
 
+    @Operation(summary = "채팅방 조회", description = "생성된 채팅방을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "성공")
     @GetMapping
     public ResponseEntity<List<ChatRoomList>> getMyChatRooms(
             @AuthenticationPrincipal CustomUserDetails userDetails
@@ -34,6 +40,8 @@ public class ChatRoomController {
         return ResponseEntity.ok(chatRoomService.getMyChatRooms(userId));
     }
 
+    @Operation(summary = "메세지 전송", description = "메세지를 전송합니다.")
+    @ApiResponse(responseCode = "200", description = "전송 성공")
     @PostMapping("/{chatRoomId}/messages")
     public ResponseEntity<ChatMessageResponse> sendMessage(
             @PathVariable("chatRoomId") Long chatRoomId,
@@ -43,6 +51,9 @@ public class ChatRoomController {
         return ResponseEntity.ok(chatMessageService.sendMessage(chatRoomId, senderId, requeset));
     }
 
+
+    @Operation(summary = "채팅메세지 조회", description = "채팅방 내의 메세지를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "성공")
     @GetMapping("/{chatRoomId}/messages")
     public ResponseEntity<List<ChatMessageList>> getChatMessages(
             @PathVariable Long chatRoomId,
@@ -52,6 +63,10 @@ public class ChatRoomController {
         return ResponseEntity.ok(chatMessageService.getChatMessages(chatRoomId, offset, limit));
     }
 
+    @Operation(summary = "메세지 삭제", description = "특정 메세지를 삭제합니다.")
+    @ApiResponse(responseCode = "204", description = "삭제 성공")
+    @ApiResponse(responseCode = "404", description = "메세지를 찾을 수 없습니다.")
+    @ApiResponse(responseCode = "403", description = "수정 또는 삭제 권한이 없습니다.")
     @DeleteMapping("/{chatRoomId}/messages/{messageId}")
     public ResponseEntity<Void> deleteChatMessage(
             @PathVariable Long chatRoomId,
@@ -63,6 +78,10 @@ public class ChatRoomController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "메세지 수정", description = "특정 메세지를 수정합니다.")
+    @ApiResponse(responseCode = "200", description = "수정 성공")
+    @ApiResponse(responseCode = "404", description = "메세지를 찾을 수 없습니다.")
+    @ApiResponse(responseCode = "403", description = "수정 또는 삭제 권한이 없습니다.")
     @PatchMapping("/{chatRoomId}/messages/{messageId}")
     public ResponseEntity<ChatMessageResponse> updateChatMessage(
             @PathVariable Long chatRoomId,
