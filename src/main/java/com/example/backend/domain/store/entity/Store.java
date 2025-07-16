db3352e1e282005a9e883be5ef5a81ce561d5f75
hwan2-99 <thh1233123@gmail.com>
2025-07-17 08:19:54 +0900

    feat: 상점의 운영 시간 도메인 분리

--- src/main/java/com/example/backend/domain/store/entity/Store.java
+++ src/main/java/com/example/backend/domain/store/entity/Store.java
@@ -1,13 +1,20 @@
 package com.example.backend.domain.store.entity;
 
+import com.example.backend.domain.global.BaseEntity;
+import com.example.backend.domain.member.entity.Member;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-public class Store {
+@NoArgsConstructor
+public class Store extends BaseEntity {
     @Id
     private Long id;
 
@@ -15,12 +22,41 @@ public class Store {
 
     private String address;
 
-    private String times;
-
     private String phone_number;
 
     private String description;
 
     @Enumerated
     private StoreCategory category;
+
+    @Embedded
+    private StoreDuration storeDuration;
+
+    @ManyToOne
+    private Member member;
+
+    public Store(String name, String address, String phone_number, String description, StoreCategory category,
+                 StoreDuration storeDuration, Member member) {
+        this(null, name, address, phone_number, description, category, storeDuration, member);
+    }
+
+    public Store(Long id, String name, String address, String phone_number, String description, StoreCategory category,
+                 StoreDuration storeDuration, Member member) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone_number = phone_number;
+        this.description = description;
+        this.category = category;
+        this.storeDuration = storeDuration;
+        this.member = member;
+    }
+
+    public LocalDate getStartDate() {
+        return storeDuration.getStartDate();
+    }
+
+    public LocalDate getEndDate() {
+        return storeDuration.getEndDate();
+    }
 }
---
+++ src/main/java/com/example/backend/domain/store/entity/StoreDuration.java
@@ -0,0 +1,27 @@
+package com.example.backend.domain.store.entity;
+
+import jakarta.persistence.Embeddable;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class StoreDuration {
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public StoreDuration(LocalDate startDate, LocalDate endDate) {
+        validate(startDate, endDate);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    private void validate(LocalDate startDate, LocalDate endDate) {
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("Start date cannot be after end date");
+        }
+    }
+}
