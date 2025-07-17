12d974b01d7aa86f2499bccda19555a8e2d3ba7d
hwan2-99 <thh1233123@gmail.com>
2025-07-17 09:04:32 +0900

    feat: 테스트 환경 분리를 위한 커스텀 어노테이션 구현

---
+++ src/test/java/support/annotation/ServiceTest.java
@@ -0,0 +1,19 @@
+package support.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import support.DatabaseCleanerExtension;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Retention(RetentionPolicy.RUNTIME)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@ExtendWith(DatabaseCleanerExtension.class)
+public @interface ServiceTest {
+}
